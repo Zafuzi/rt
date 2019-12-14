2b3b98e23e9574d77c977b41bd0e7f1e814c47a5
zafuzi <zacharyfoutz@gmail.com>
2019-12-14 00:26:00 -0800

    Fixed movement speed again

--- game/game.js
+++ game/game.js
@@ -1,3 +1,55 @@
+// -------------------------------------------------------
+// Some math stuff
+// -------------------------------------------------------
+let max = Math.max;
+let min = Math.min;
+let sqrt = Math.sqrt;
+let sin = Math.sin;
+let cos = Math.cos;
+let floor = Math.floor;
+let abs = Math.abs;
+let round = Math.round;
+let atan2 = Math.atan2;
+let PI = Math.PI;
+let PI2 = PI * 2;
+let PIH = PI / 2;
+
+// apply some stupid semi-newtonian motion to a squid
+function newton(sq) {
+	// apply velocity
+	sq.x = (sq.x + sq.vx);
+	sq.y = (sq.y + sq.vy);
+	// apply gravity
+	sq.vx = sq.vx + sq.gx;
+	sq.vy = sq.vy + sq.gy;
+
+	// apply rotational velocity, acceleration, and friction 
+	sq.r = sq.r + sq.vr;
+	if(sq.r > PI2) { sq.r = sq.r - PI2 };
+	if(sq.r < 0) { sq.r = sq.r + PI2 };
+	sq.vr = sq.vr + sq.ra;
+	sq.vr = sq.vr - sq.rf;
+}
+
+function screenWrap(sq) {
+	if( sq.x >= game.SW ) { 
+		sq.x = 0;
+	}
+	else
+	if( sq.x < 0 ) {
+		sq.x = game.SW - 1
+	}
+
+	if( sq.y >= game.SH ) { 
+		sq.y = 0;
+	}
+	else
+	if( sq.y < 0 ) {
+		sq.y = game.SH - 1
+	}
+
+}
+
 app_keyup = function(key) {
 	game.keys_down[key] = 0;
 }
@@ -28,16 +80,16 @@ var game = {
 						music.volume(0);
 						break;
 				 	case 'w':
-						game.player.tgty = game.player.y - game.player.speed;
+						game.player.vy = -game.player.speed;
 						break;
 					case 's':
-						game.player.tgty = game.player.y + game.player.speed;
+						game.player.vy = game.player.speed; 
 						break;
 				 	case 'a':
-						game.player.tgtx = game.player.x - game.player.speed;
+						game.player.vx = -game.player.speed;
 						break;
 					case 'd':
-						game.player.tgtx = game.player.x + game.player.speed;
+						game.player.vx = game.player.speed;
 						break;
 					default: break;
 				}
@@ -79,20 +131,21 @@ var game = {
 			//sq_debug(true);
 			sq_useCanvas("mycanvas", game.SW, game.SH);
 
-
-			console.log(game.SW);
 			game.player = sq_create(sq_getImage("rt.png"), game.SW * 0.5, game.SH * 0.5);
 			game.player.alive = true;
 
-			game.player.tgtx = game.player.x;
-			game.player.tgty = game.player.y;
-
-			game.player.speed = 30;
+			game.player.speed = 3;
 
 			game.player.tick = function() {
 				let sq = this;
-				this.vx = (this.tgtx - this.x) * 0.1;
-				this.vy = (this.tgty - this.y) * 0.1;
+				
+				newton(this);
+
+				sq.vx = sq.vx * 0.1;		// reduce my velocity by 10% (?)
+				sq.vy = sq.vy * 0.1;
+
+				screenWrap(this);
+
 				sq_tick(this);
 			}
 
